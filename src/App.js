519303646686e2afb1140f11b0fb4ca948ae8cbf
darshanmarviya <dmaraviya2003@gmail.com>
2024-04-09 12:03:21 +0530

    update of resume

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ import logo from "./logo.svg";
 import "./App.css";
 import Resume from './Resumes.js';
 //import Suggestion from "./Suggestions";
-const allData = require("./data.json");
+// const allData = require("./data.json");
+// const allData = require("./resumedata.json");
+const allData = require('./resumedata.json');
 
 function App() {
   console.log(allData);
   return (
     <>
       {
+        allData.map(function (rdata){
+          return <Resume r_name={rdata.name}
+          r_address={rdata.address}
+          r_phone={rdata.phone}
+          r_email={rdata.email}
+          r_linkedin={rdata.linkedin}
+          r_website={rdata.website}
+          r_additionalskill={rdata.additionalskill}
+          r_professionalsummary={rdata.professionalsummary}
+          r_workexprience={rdata.workexprience}
+          r_educationdetails={rdata.educationdetails}
+          r_language={rdata.language}
+          r_workexdate={rdata.workexdate}
+          r_educationdate={rdata.educationdate}/>
+        })
+        
       // allData.map(function (suggest) {
       //   return <Suggestion suggestionPic={suggest.suggestPic} suggestionName={suggest.suggestName} />
         
       // })
       // }
-      <Resume />
-  }
+      
+      }
     </>
   );  
 }
--- src/Resumes.js
+++ src/Resumes.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Resume() {
+function Resume(props) {
   return (
     <>
     <div style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
@@ -14,7 +14,7 @@ function Resume() {
     {/* Name div */}
       <div style={{color:'white', margin:'10%', fontSize:'14px'}}>
       <header>
-        <h1 style={{marginTop:'-50px',marginBottom:'35px'}}>Darshan Maraviya</h1>
+        <h1 style={{marginTop:'-50px',marginBottom:'35px'}}>{props.r_name}</h1>
       </header>
 
     {/* Contact */}
@@ -23,19 +23,19 @@ function Resume() {
         <h4 style={{marginTop:'-28px'}}>_____________________________________________</h4>
 
         <h4>Address : </h4>
-        <p style={{marginTop:'-20px'}}>177 Great Portalnd Street, London W5W6PQ</p>
+        <p style={{marginTop:'-20px'}}>{props.r_address}</p>
 
         <h4>Phone : </h4>
-        <p style={{marginTop:'-20px'}}>+1 (970) 333-3833</p>
+        <p style={{marginTop:'-20px'}}>{props.r_phone}</p>
 
         <h4>Email : </h4>
-        <p style={{marginTop:'-20px'}}>susan.willams@gmail.com</p>
+        <p style={{marginTop:'-20px'}}>{props.r_email}</p>
 
         <h4>Linkedin : </h4>
-        <p style={{marginTop:'-20px'}}>linkedin.com/susan.willams</p>
+        <p style={{marginTop:'-20px'}}>{props.r_linkedin}</p>
 
         <h4>Website : </h4>
-        <p style={{marginTop:'-20px'}}>https://www.coolfreecv.com</p>
+        <p style={{marginTop:'-20px'}}>{props.r_website}</p>
       </section>
 
 
@@ -44,18 +44,8 @@ function Resume() {
         <h3 style={{fontSize:'18px'}}>Additional Skill</h3>
         <h4 style={{marginTop:'-28px'}}>_____________________________________________</h4>
 
-        <p style={{marginTop:'20px'}}> - Results-oriented</p>
-        <p style={{marginTop:'-11px'}}> - Origanisational capacity</p>
-        <p style={{marginTop:'-11px'}}> - Revenue generation</p>
-        <p style={{marginTop:'-11px'}}> - Business development</p>
-        <p style={{marginTop:'-11px'}}> - Effective marketing</p>
-        <p style={{marginTop:'-11px'}}> - Organisational capacity</p>
-        <p style={{marginTop:'-11px'}}> - Operability and commitment</p>
-        <p style={{marginTop:'-11px'}}> - Ability to motivate staff</p>
-        <p style={{marginTop:'-11px'}}> - Maintain good relations</p>
-        <p style={{marginTop:'-11px'}}> - Good manners</p>
+        <p style={{marginTop:'20px',whiteSpace:'pre-wrap'}}>{props.r_additionalskill}</p>
 
-        
       </section>
 
       </div>
@@ -71,7 +61,7 @@ function Resume() {
     <section style={{marginTop:'30px',marginLeft:'10px'}}>
         <h3 style={{fontSize:'18px'}}>Professional Summary</h3>
         <h4 style={{marginTop:'-28px'}}>_______________________________________________________________________________________________________</h4>
-        <p style={{marginTop:'-20px',marginLeft:'13px', fontSize:'0.9rem',marginRight:'13px'}}> Hiring managers skim through resumes before deciding to read them, so candidates who grab their attention from the start stand a better chance of grabbing their attention. This is why you should always include a brief paragraph in your resume header that summarizes your strengths as a candidate.Depending on your experience level, you can choose between a resume summary or a resume objective.Resume objectives focus on professional goals, skills, and motivation for applying for the job, so they’re perfect for students, recent graduates, and other entry-level workers.A resume summary, on the other hand, is perfect for seasoned professionals, as it focuses more on your past work experience and how it relates to the job you’re applying for. So, if you already have some work experience, we recommend sticking to a resume summary.If you’re embarking on a career change, on the other hand, you can choose between a or a resume objective, depending on how your previous skills and work experience relate to the job. </p>
+        <p style={{marginTop:'-20px',marginLeft:'13px', fontSize:'0.9rem',marginRight:'13px'}}>{props.r_professionalsummary}</p>
     </section>
 
 
@@ -79,24 +69,10 @@ function Resume() {
     <section style={{marginTop:'40px',marginLeft:'10px'}}>
         <h3 style={{fontSize:'18px'}}>Work Experience</h3>
         <h4 style={{marginTop:'-28px'}}>_______________________________________________________________________________________________________</h4>
-        <h4 style={{textAlign:'right',marginTop:'0px',marginRight:'20px',marginBottom:'0px',marginLeft:'0px'}}>5/2020 to 3/2023</h4>
-        <b style={{marginLeft:'13px'}}>Store Manage LUXURY CAR CENTER, New York</b>
+        <h4 style={{textAlign:'right',marginTop:'0px',marginRight:'20px',marginBottom:'0px',marginLeft:'0px'}}>{props.r_workexdate}</h4>
+        <b style={{marginLeft:'13px'}}>Android Application</b>
         <p style={{marginLeft:'18px', fontSize:'0.9rem'}}>
-            <p style={{marginTop:'-11px'}}>- Motivate and coach employees to meet service, sales, and repair goals.</p>
-            <p style={{marginTop:'-11px'}}>- Create and modify employee schedules with service levels in mind.</p>
-            <p style={{marginTop:'-11px'}}>- Recruit and hire top mechanics, service adviors, and sales people.</p>
-            <p style={{marginTop:'-11px'}}>- Maintain detailed logs and reports of services performed, profit, and budget information.</p>
-            <p style={{marginTop:'-11px'}}>- Help out in sales and repair areas as needed and maintain comprehensive current knowledge of operations.</p>
-        </p>
-
-        <h4 style={{textAlign:'right',marginTop:'0px',marginRight:'20px',marginBottom:'0px',marginLeft:'0px'}}>5/2020 to 3/2023</h4>
-        <b style={{marginLeft:'13px'}}>Store Manage LUXURY CAR CENTER, New York</b>
-        <p style={{marginLeft:'18px', fontSize:'0.9rem'}}>
-            <p style={{marginTop:'-11px'}}>- Create and modify employee schedules with service levels in mind.</p>
-            <p style={{marginTop:'-11px'}}>- Maintain detailed logs and reports of services performed, profit, and budget information.</p>
-            <p style={{marginTop:'-11px'}}>- Motivate and coach employees to meet service, sales, and repair goals.</p>
-            <p style={{marginTop:'-11px'}}>- Recruit and hire top mechanics, service adviors, and sales people.</p>
-            <p style={{marginTop:'-11px'}}>- Help out in sales and repair areas as needed and maintain comprehensive current knowledge of operations.</p>
+            <p style={{marginTop:'-11px'}}>{props.r_workexprience}</p>
         </p>
     </section>
 
@@ -105,15 +81,9 @@ function Resume() {
     <section style={{marginTop:'30px',marginLeft:'10px'}}>
         <h3 style={{fontSize:'18px'}}>Education Details</h3>
         <h4 style={{marginTop:'-28px'}}>_______________________________________________________________________________________________________</h4>
-        <h4 style={{textAlign:'right',marginTop:'0px',marginRight:'20px',marginBottom:'0px',marginLeft:'0px'}}>2019 - 2021</h4>
+        <h4 style={{textAlign:'right',marginTop:'0px',marginRight:'20px',marginBottom:'0px',marginLeft:'0px'}}>{props.r_educationdate}</h4>
         <p style={{marginLeft:'13px', fontSize:'0.9rem',marginTop:'0px'}}>
-          <b>Bachelor of Science</b>
-          <p style={{marginTop:'0px'}}>Automotive Technology - Technical University, New York</p>
-        </p>
-
-        <h4 style={{textAlign:'right',marginTop:'0px',marginRight:'20px',marginBottom:'0px',marginLeft:'0px'}}>2021 - 2023</h4>
-        <p style={{marginLeft:'13px', fontSize:'0.9rem',marginTop:'0px'}}>
-          <b>Master of Science</b>
+          <b>{props.r_educationdetails}</b>
           <p style={{marginTop:'0px'}}>Automotive Technology - Technical University, New York</p>
         </p>
     </section>
@@ -124,9 +94,7 @@ function Resume() {
         <h3 style={{fontSize:'18px'}}>Languages</h3>
         <h4 style={{marginTop:'-28px'}}>_______________________________________________________________________________________________________</h4>
         <p style={{marginLeft:'13px', fontSize:'0.9rem'}}>
-          <p style={{marginTop:'-11px'}}> - Spanish</p>
-          <p style={{marginTop:'-11px'}}> - English</p>
-          <p style={{marginTop:'-11px'}}> - Hindi</p>
+          <p style={{marginTop:'-11px'}}>{props.r_language}</p>
         </p>
     </section>
 
